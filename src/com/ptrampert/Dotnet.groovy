29cf834f71224cc4a52d943d98b76487de8f7b27
Paul Trampert <paul.trampert@gmail.com>
2016-11-04 23:04:20 -0400

    Some dotnet params are optional.

--- src/com/ptrampert/Dotnet.groovy
+++ src/com/ptrampert/Dotnet.groovy
@@ -9,22 +9,18 @@ def shell(command) {
     }
 }
 
-def build(project, opts) {
-    if (!project) {
-        project = "**/project.json"
-    }
-
+def build(project = "**/project.json", opts = "") {
     shell "dotnet build ${project} ${opts}"
 }
 
-def restore() {
-    shell "dotnet restore"
+def restore(project = "", opts = "") {
+    shell "dotnet restore ${project} ${opts}"
 }
 
-def pack(project, opts) {
+def pack(project, opts = "") {
     shell "dotnet pack ${project} ${opts}"
 }
 
-def publish(project, opts) {
+def publish(project, opts = "") {
     shell "dotnet publish ${project} ${opts}"
 }
