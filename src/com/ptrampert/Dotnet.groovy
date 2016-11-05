7180c8718c45f5818fbc08add3cbe2c3a3291d24
Paul Trampert <paul.trampert@gmail.com>
2016-11-04 22:54:57 -0400

    Fix pipeline?

---
+++ src/com/ptrampert/Dotnet.groovy
@@ -0,0 +1,30 @@
+package com.ptrampert
+
+def shell(command) {
+    if (isUnix()) {
+        sh command
+    }
+    else {
+        bat command
+    }
+}
+
+def build(project, opts) {
+    if (!project) {
+        project = "**/project.json"
+    }
+
+    shell "dotnet build ${project} ${opts}"
+}
+
+def restore(project, opts) {
+    shell "dotnet restore ${project} ${opts}"
+}
+
+def pack(project, opts) {
+    shell "dotnet pack ${project} ${opts}"
+}
+
+def publish(project, opts) {
+    shell "dotnet publish ${project} ${opts}"
+}
--- vars/nugetPipeline.groovy
+++ vars/nugetPipeline.groovy
@@ -1,9 +1,12 @@
+import com.ptrampert.Dotnet
+
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
+    def dotnet = new Dotnet()
     node {
         try {
             stage("Update Sources") {
