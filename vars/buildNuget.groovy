d6256d7b93031db7045bcfc19c2ca621bccdb3f4
Paul Trampert <paul.trampert@gmail.com>
2016-11-06 18:15:44 -0500

    Iteration.

--- vars/buildNuget.groovy
+++ vars/buildNuget.groovy
@@ -1,31 +1,49 @@
+/**
+* Builds and tests a nuget package. Assumes nunit3 as the test runner.
+* Config Values:
+*   project: The project to Build
+*   testProject: The test project for running tests. Defaults to '${project}.Test'
+*   artifactDir: The directory to write artifacts out to. Defaults to 'Artifacts'
+*   isRelease: True if performing a release build. False if pre-release. 
+*/
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
+    def project = config.project
+    def testProject = config.testProject ? config.testProject : "${project}.Test"
+    def artifactDir = config.artifactDir ? config.artifactDir : "Artifacts"
+    def isRelease = config.isRelease
+
     node {
         try {
             stage("Update Sources") {
                 checkout scm
+                if (!fileExists(artifactDir)) {
+                    shell "mkdir ${artifactDir}"
+                }
             }
 
             stage("Build") {
-                if (!fileExists('Artifacts')) {
-                    shell "mkdir Artifacts"
-                }
                 dotnetBuild()
             }
 
             stage("Test") {
                 catchError {
-                    shell "dotnet test ${config.project}.Test --result Artifacts/TestResults.xml"
+                    shell "dotnet test ${testProject} --result ${artifactDir}/TestResults.xml"
                 }
             }
 
             stage("Package") {
-                def shortBranch = env.BRANCH_NAME.take(10)
-                shell "dotnet pack ${config.project} --output Artifacts --version-suffix ${shortBranch}-${env.BUILD_NUMBER}"
+                if (isRelease) {
+                    shell "dotnet pack ${project} --output ${artifactDir}"
+                }
+                else {
+                    def shortBranch = env.BRANCH_NAME.take(10)
+                    shell "dotnet pack ${project} --output ${artifactDir} --version-suffix ${shortBranch}-${env.BUILD_NUMBER}"
+                }
             }
 
             stage("Reporting") {
@@ -39,10 +57,10 @@ def call(body) {
                                 [$class: 'FailedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '0'],
                                 [$class: 'SkippedThreshold', failureNewThreshold: '', failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']],
                         tools         : [
-                                [$class: 'CustomType', customXSL: 'nunit3-xunit.xslt', deleteOutputFiles: true, failIfNotNew: true, pattern: 'Artifacts/TestResults.xml', skipNoTestFiles: false, stopProcessingIfError: true]]
+                                [$class: 'CustomType', customXSL: 'nunit3-xunit.xslt', deleteOutputFiles: true, failIfNotNew: true, pattern: '${artifactDir}/TestResults.xml', skipNoTestFiles: false, stopProcessingIfError: true]]
                 ])
-                archiveArtifacts artifacts: 'Artifacts/*.nupkg', excludes: 'Artifacts/*.symbols.nupkg'
-                stash excludes: 'Artifacts/*.symbols.nupkg', includes: 'Artifacts/*.nupkg', name: 'nupkg'
+                archiveArtifacts artifacts: '${artifactDir}/*.nupkg', excludes: '${artifactDir}/*.symbols.nupkg'
+                stash excludes: '${artifactDir}/*.symbols.nupkg', includes: '${artifactDir}/*.nupkg', name: 'nupkg'
             }
         } catch (any) {
             currentBuild.result = "FAILURE"
--- vars/nugetPipeline.groovy
+++ vars/nugetPipeline.groovy
@@ -1,3 +1,11 @@
+/**
+* Configures a full nuget build, test, and deployment pipeline. Assumes git as the source control.
+* Config Values:
+*   project: The project to Build
+*   testProject: The test project for running tests. Defaults to '${project}.Test'
+*   artifactDir: The directory to write artifacts out to. Defaults to 'Artifacts'
+*   isRelease: True if performing a release build. False if pre-release. 
+*/
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
@@ -7,5 +15,7 @@ def call(body) {
     buildNuget {
         project = config.project
         notificationRecipients = config.notificationRecipients
+        artifactDir = config.artifactDir
+        isRelease = config.isRelease
     }
 }
