157b778333e5b35e6bdff6b541ea7f0ad1d66d97
Paul Trampert <paul.trampert@gmail.com>
2016-11-06 19:31:38 -0500

    Setup build properties in nugetPipeline.

--- vars/nugetPipeline.groovy
+++ vars/nugetPipeline.groovy
@@ -1,10 +1,13 @@
 /**
 * Configures a full nuget build, test, and deployment pipeline. Assumes git as the source control.
+* Build Parameters:
+*   ReleaseVersion: The release version. This will be made available to any build scripts via an environment variable of the same name.
+*   NextVersion: The next working version. This will be made available to any build scripts via an environment variable of the same name.
+*   IsRelease: Indicates whether to build a release package or a pre-release package. 
 * Config Values:
 *   project: The project to Build
 *   testProject: The test project for running tests. Defaults to "${project}.Test"
 *   artifactDir: The directory to write artifacts out to. Defaults to "Artifacts"
-*   isRelease: True if performing a release build. False if pre-release. 
 */
 def call(body) {
     def config = [:]
@@ -12,10 +15,24 @@ def call(body) {
     body.delegate = config
     body()
 
+    properties([
+        parameters([
+            string(defaultValue: '', description: '', name: 'ReleaseVersion'), 
+            string(defaultValue: '', description: '', name: 'NextVersion'), 
+            booleanParam(defaultValue: false, description: '', name: 'IsRelease')
+        ]), 
+        pipelineTriggers([])
+    ])
+
+    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([])])
+
+    env.ReleaseVersion = params.ReleaseVersion
+    env.NextVersion = params.NextVersion
+
     buildNuget {
         project = config.project
         notificationRecipients = config.notificationRecipients
         artifactDir = config.artifactDir
-        isRelease = config.isRelease
+        isRelease = params.isRelease
     }
 }
