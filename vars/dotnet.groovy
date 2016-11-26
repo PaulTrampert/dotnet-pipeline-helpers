9770a86d593f9bd75a5ad6a375bc2a23dc3034f6
Paul Trampert <paul.trampert@gmail.com>
2016-11-25 19:43:01 -0500

    WTF Groovy?

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -1,5 +1,9 @@
 def call(String command, String project, Map args) {
     def shellCommand = "dotnet ${command} ${project}"
-    args.keySet().each { shellCommand = "${shellCommand} ${it} ${args[it] == true ? '' : args[it]}"}
+    echo "Command Line Arg Keys: ${args.keySet()}"
+    args.keySet().each {
+        echo "Cmd Line Arg: ${it} ${args[it]}"
+        shellCommand = "${shellCommand} ${it} ${args[it] == true ? '' : args[it]}"
+    }
     shell shellCommand
 }
