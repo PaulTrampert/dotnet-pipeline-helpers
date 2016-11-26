e68ac36dc8fb3fbdcdd9ae38af7ccf2e9e85c921
Paul Trampert <paul.trampert@gmail.com>
2016-11-25 19:48:39 -0500

    Remove debugging code.

--- vars/buildNuget.groovy
+++ vars/buildNuget.groovy
@@ -37,10 +37,10 @@ def call(body) {
                 packArgs.put('--version-suffix', "${env.BRANCH_NAME.take(10)}-${env.BUILD_NUMBER}")
             }
             if (isOpenSource) {
-                packArgs.put('--include-source', true)
+                packArgs.put('--include-source', "")
             }
             if (releaseVersion) {
-                packArgs.put("/p:VersionPrefix=${releaseVersion}", true)
+                packArgs.put("/p:VersionPrefix=${releaseVersion}", "")
             }
             dotnetPack(project, packArgs)
         }
--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -1,9 +1,7 @@
 def call(String command, String project, Map args) {
     def shellCommand = "dotnet ${command} ${project}"
-    echo "Command Line Arg Keys: ${args.keySet()}"
     for (k in args.keySet()) {
-        echo "Cmd Line Arg: ${k} ${args[k]}"
-        shellCommand = "${shellCommand} ${k} ${args[k] == true ? '' : args[k]}"
+        shellCommand = "${shellCommand} ${k} ${args[k]}"
     }
     shell shellCommand
 }
