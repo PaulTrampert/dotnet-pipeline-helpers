782c994c4e333f3015535aa30e6babf2a713e356
Paul Trampert <paul.trampert@gmail.com>
2016-11-25 21:43:27 -0500

    unstash is necessary due to workspace clearing at the end of the build.

--- vars/deployNuget.groovy
+++ vars/deployNuget.groovy
@@ -6,6 +6,7 @@ def call(body) {
 
     try {
         stage("Publish Package") {
+            unstash 'nupkg'
             withCredentials([[$class: 'StringBinding', credentialsId: config.nugetCredentialsId, variable: 'NUGET_API_KEY']]) {
                 shell "nuget push **/*.nupkg -ApiKey ${env.NUGET_API_KEY} -Source ${config.nugetServer}"
             }
