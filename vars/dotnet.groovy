a4116e3c3a6923a03e0ea5b2d3e2f008c7683f8a
Paul Trampert <paul.trampert@gmail.com>
2016-11-25 20:07:32 -0500

    Fix issue with null args.

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -1,7 +1,7 @@
 def call(String command, String project, Map args) {
     def shellCommand = "dotnet ${command} ${project}"
     for (k in args.keySet()) {
-        shellCommand = "${shellCommand} ${k} ${args[k]}"
+        shellCommand = "${shellCommand} ${k} ${args[k] ?: ''}"
     }
     shell shellCommand
 }
