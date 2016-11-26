69ee077046c4213990e27b0cd4e1f6cc62df1bf9
Paul Trampert <paul.trampert@gmail.com>
2016-11-25 19:28:34 -0500

    Check args keyset before each shell command.

--- vars/dotnet.groovy
+++ vars/dotnet.groovy
@@ -1,5 +1,6 @@
 def call(String command, String project, Map args) {
     def shellCommand = "dotnet ${command} ${project}"
+    echo "${args.keySet()}"
     args.each {k, v -> shellCommand = "${shellCommand} ${k} ${v}"}
     shell shellCommand
 }
