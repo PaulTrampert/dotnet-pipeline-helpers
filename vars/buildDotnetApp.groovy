bce7570a7c54bface4e106229141b952bee48c7c
Paul Trampert <paul.trampert@gmail.com>
2016-12-17 11:58:23 -0500

    Dotnet app pipeline.

---
+++ vars/buildDotnetApp.groovy
@@ -0,0 +1,58 @@
+/**
+ * Created by pault on 12/17/2016.
+ */
+/**
+ * Builds and tests a nuget package. Assumes nunit3 as the test runner.
+ * Config Values:
+ *   project: The project to Build
+ *   testProject: The test project for running tests. Defaults to '${project}.Test'
+ *   isRelease: True if performing a release build. False if pre-release.
+ */
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def testProjects = config.testProjects
+    def publishProjects = config.publishProjects
+    def releaseVersion = config.releaseVersion
+
+    try {
+
+        stage("Build") {
+            def buildArgs = []
+            if (releaseVersion) {
+                buildArgs << "/p:VersionPrefix=${releaseVersion}"
+            }
+            dotnetBuild('', buildArgs)
+        }
+
+        if (testProjects) {
+            stage("Test") {
+                for(def proj : testProjects) {
+                    dotnetTest("${proj}/${proj}.csproj", ['--logger', 'trx', '--noBuild'])
+                }
+            }
+        }
+
+        if (publishProjects) {
+            stage("Publish") {
+                for(def proj : publishProjects) {
+                    dotnetPublish("${proj}/${proj}.csproj", ['--no-build'])
+                }
+            }
+        }
+
+        stage("Reporting") {
+            reportMSTestResults("**/*.trx")
+            archiveArtifacts artifacts: "**/publish/**"
+        }
+    } catch (any) {
+        currentBuild.result = "FAILURE"
+        throw any
+    } finally {
+        deleteDir()
+        emailext attachLog: true, recipientProviders: [[$class: 'CulpritsRecipientProvider']]
+    }
+}
---
+++ vars/dotnetAppPipeline.groovy
@@ -0,0 +1,48 @@
+/**
+ * Created by pault on 12/17/2016.
+ */
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    if (env.BRANCH_NAME) {
+        properties([
+                buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5'))
+        ])
+    }
+    else {
+        properties([
+                buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
+                parameters([
+                        booleanParam(defaultValue: true, description: '', name: 'IS_RELEASE'),
+                        string(defaultValue: '', description: '', name: 'RELEASE_VERSION'),
+                        string(defaultValue: '', description: '', name: 'RELEASE_NOTES')
+                ]),
+                pipelineTriggers([])
+        ])
+    }
+
+
+    def RELEASE_VERSION = params.RELEASE_VERSION
+    def RELEASE_NOTES = params.RELEASE_NOTES
+    def IS_RELEASE = params.IS_RELEASE
+
+    echo "Building ${env.BRANCH_NAME}"
+
+    node{
+        stage("Update Sources") {
+            checkout scm
+        }
+        withEnv(["PackageReleaseNotes=\"${RELEASE_NOTES}\""]) {
+            buildDotnetApp {
+                publishProjects = config.publishProjects
+                testProjects = config.testProjects
+                publishProjects = config.publishProjects
+                isRelease = IS_RELEASE
+                releaseVersion = RELEASE_VERSION
+            }
+        }
+    }
+}
