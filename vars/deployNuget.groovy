f230a7ac97e45aa88d534e17155165610a3c4bf8
Paul Trampert <paul.trampert@gmail.com>
2017-02-12 10:53:16 -0500

    Security tweaks on deployNuget.groovy.

--- vars/deployNuget.groovy
+++ vars/deployNuget.groovy
@@ -8,7 +8,7 @@ def call(body) {
         stage("Publish Package") {
             unstash 'nupkg'
             def nugetCredentialsId = config.nugetCredentialsId
-            def nugetServer = config.nugetServer
+            def nugetServer = config.nugetServer ?: 'https://api.nuget.org/v3/index.json'
             def symbolCredId = config.symbolsCredentialsId ?: nugetCredentialsId
             def symbolServer = config.symbolServer
             echo "Credentials ID: ${config.nugetCredentialsId ?: '<default>'}"
@@ -16,27 +16,23 @@ def call(body) {
             echo "Symbols Credentials ID: ${symbolCredId ?: '<default>'}"
             echo "Symbol Server: ${symbolServer ?: '<default>'}"
 
-            def args = []
-            if (nugetCredentialsId) {
-                withCredentials([string(credentialsId: nugetCredentialsId, variable: 'NUGET_API_KEY')]) {
+            withCredentials([string(credentialsId: nugetCredentialsId, variable: 'NUGET_API_KEY')]) {
+                withCredentials([string(credentialsId: symbolCredId, variable: 'SYMBOL_API_KEY')]) {
+                    def args = []
                     args << "--api-key ${env.NUGET_API_KEY}"
+                    args << "--source ${nugetServer}"
+                    if (config.isOpenSource) {
+                        args << "--symbol-api-key ${env.SYMBOL_API_KEY}"
+                    }
+                    if (symbolServer && config.isOpenSource) {
+                        args << "--symbol-source ${symbolServer}"
+                    }
+                    if (!config.isOpenSource) {
+                        args << "--no-symbols true"
+                    }
+                    dotnetNugetPush("**/*.nupkg", args)
                 }
             }
-            if (nugetServer) {
-                args << "--source ${nugetServer}"
-            }
-            if (symbolCredId) {
-                withCredentials([string(credentialsId: nugetCredentialsId, variable: 'SYMBOL_API_KEY')]) {
-                    args << "--symbol-api-key ${env.SYMBOL_API_KEY}"
-                }
-            }
-            if (symbolServer) {
-                args << "--symbol-source ${symbolServer}"
-            }
-            if (!config.isOpenSource) {
-                args << "--no-symbols true"
-            }
-            dotnetNugetPush("**/*.nupkg", args)
         }
     } catch (any) {
         currentBuild.result = "FAILURE"
