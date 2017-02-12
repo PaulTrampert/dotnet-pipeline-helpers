3dd637ab1349133b25afdbdf8ccd9e512a2c77c1
Paul Trampert <paul.trampert@gmail.com>
2017-02-12 11:04:41 -0500

    Don't try to push symbols if no symbol server is configured.

--- vars/deployNuget.groovy
+++ vars/deployNuget.groovy
@@ -11,10 +11,10 @@ def call(body) {
             def nugetServer = config.nugetServer ?: 'https://api.nuget.org/v3/index.json'
             def symbolCredId = config.symbolsCredentialsId ?: nugetCredentialsId
             def symbolServer = config.symbolServer
-            echo "Credentials ID: ${config.nugetCredentialsId ?: '<default>'}"
-            echo "Nuget Server: ${config.nugetServer ?: '<default>'}"
+            echo "Credentials ID: ${nugetCredentialsId ?: '<default>'}"
+            echo "Nuget Server: ${nugetServer ?: '<default>'}"
             echo "Symbols Credentials ID: ${symbolCredId ?: '<default>'}"
-            echo "Symbol Server: ${symbolServer ?: '<default>'}"
+            echo "Symbol Server: ${symbolServer ?: '<none>'}"
 
             withCredentials([string(credentialsId: nugetCredentialsId, variable: 'NUGET_API_KEY')]) {
                 withCredentials([string(credentialsId: symbolCredId, variable: 'SYMBOL_API_KEY')]) {
@@ -27,7 +27,7 @@ def call(body) {
                     if (symbolServer && config.isOpenSource) {
                         args << "--symbol-source ${symbolServer}"
                     }
-                    if (!config.isOpenSource) {
+                    if (!symbolServer || !config.isOpenSource) {
                         args << "--no-symbols true"
                     }
                     dotnetNugetPush("**/*.nupkg", args)
