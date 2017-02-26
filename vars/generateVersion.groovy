1f2bc5605d32365d2d346ae3cc45a8edd3f3af2b
Paul Trampert <paul.trampert@gmail.com>
2017-02-26 17:32:33 -0500

    Debug echos.

--- vars/generateVersion.groovy
+++ vars/generateVersion.groovy
@@ -9,7 +9,12 @@ def call() {
     else {
         describeString = bat returnStdout: true, script: 'git describe --tags'
     }
+    echo "describeString = ${describeString}"
     def semver = SemVer.Parse describeString
     semver.minor++
+    echo "semver.major = ${semver.major}"
+    echo "semver.minor = ${semver.minor}"
+    echo "semver.patch = ${semver.patch}"
+    echo "semver.toString() = ${semver.toString()}"
     return semver.toString()
 }
